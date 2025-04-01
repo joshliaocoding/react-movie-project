df12f7c0985b62e08522e8cf180d3bdec9c38c7e
joshliaocoding <joshliao0607@gmail.com>
2025-04-01 20:23:55 +0800

    Create Home.jsx, Favorite.jsx and modify some files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
+
   return (
     <>
+      {movieNum === 1 && <MovieCard movie={{title: "The last of us", release_data: "2024"}}/>}
     </>
   )
 }
