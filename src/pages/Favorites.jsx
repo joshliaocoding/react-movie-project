3892990f9e5dc59d405a6c5403961766cabccebb
joshliaocoding <joshliao0607@gmail.com>
2025-04-02 10:44:41 +0800

    Create css styling for the project.

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,7 @@
+import "../css/Favorites.css"
+
 function Favorites(){
-    return <div className="favorite-empty">
+    return <div className="favorites-empty">
         <h2>No Favorite Movies Yet</h2>
         <p>Start adding movies to your favorites and they will appear here.</p>
     </div>
